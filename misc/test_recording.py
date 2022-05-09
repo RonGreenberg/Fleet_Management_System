1248373cd22a28f1d173801f3c0a219e246f1ad1
Ron Greenberg <89278943+RonGreenberg@users.noreply.github.com>
2022-05-09 23:01:47 +0300

    Added --fdm=null to comment

--- misc/test_recording.py
+++ misc/test_recording.py
@@ -1,8 +1,9 @@
 # This script is a client that connects to a TCP server opened by FlightGear on port 5400, in order to send line by line from a CSV file and show flight recording.
 # Copy the playback_small.xml file to the \data\Protocol folder inside the location where FlightGear is installed, if you haven't already.
 # For example: C:\Program Files\FlightGear 2020.3\data\Protocol
-# Open FlightGear and add the following line to Additional Settings before starting the simulator:
+# Open FlightGear and add the following two lines to Additional Settings before starting the simulator:
 # --generic=socket,in,10,127.0.0.1,5400,tcp,playback_small
+# --fdm=null
 # Start the simulator. When it finishes loading, run this script.
 
 import socket
