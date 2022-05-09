08a56ba2a47d22cd65038190bc32b3c9f9afa7d2
Ron Greenberg <89278943+RonGreenberg@users.noreply.github.com>
2022-05-09 22:32:15 +0300

    Added script to generate database

---
+++ backend/generate_db.sql
@@ -0,0 +1,58 @@
+-- MySQL Workbench Forward Engineering
+
+SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0;
+SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0;
+SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='ONLY_FULL_GROUP_BY,STRICT_TRANS_TABLES,NO_ZERO_IN_DATE,NO_ZERO_DATE,ERROR_FOR_DIVISION_BY_ZERO,NO_ENGINE_SUBSTITUTION';
+
+-- -----------------------------------------------------
+-- Schema mydb
+-- -----------------------------------------------------
+-- -----------------------------------------------------
+-- Schema fleet_management_system
+-- -----------------------------------------------------
+
+-- -----------------------------------------------------
+-- Schema fleet_management_system
+-- -----------------------------------------------------
+CREATE SCHEMA IF NOT EXISTS `fleet_management_system` DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci ;
+USE `fleet_management_system` ;
+
+-- -----------------------------------------------------
+-- Table `fleet_management_system`.`airplanes`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `fleet_management_system`.`airplanes` (
+  `planeID` VARCHAR(7) NOT NULL,
+  `model` VARCHAR(45) NOT NULL,
+  `dateAdded` DATE NOT NULL,
+  PRIMARY KEY (`planeID`))
+ENGINE = InnoDB
+DEFAULT CHARACTER SET = utf8mb4
+COLLATE = utf8mb4_0900_ai_ci;
+
+
+-- -----------------------------------------------------
+-- Table `fleet_management_system`.`flights`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `fleet_management_system`.`flights` (
+  `flightID` INT NOT NULL AUTO_INCREMENT,
+  `planeID` VARCHAR(7) NOT NULL,
+  `csvFileName` VARCHAR(20) NOT NULL,
+  `startTime` DATETIME NOT NULL,
+  `endTime` DATETIME NOT NULL,
+  `from` VARCHAR(25) NOT NULL,
+  `to` VARCHAR(25) NOT NULL,
+  `maxAltitude` FLOAT NOT NULL,
+  `maxSpeed` FLOAT NOT NULL,
+  PRIMARY KEY (`flightID`),
+  INDEX `planeID_idx` (`planeID` ASC) VISIBLE,
+  CONSTRAINT `planeID`
+    FOREIGN KEY (`planeID`)
+    REFERENCES `fleet_management_system`.`airplanes` (`planeID`))
+ENGINE = InnoDB
+DEFAULT CHARACTER SET = utf8mb4
+COLLATE = utf8mb4_0900_ai_ci;
+
+
+SET SQL_MODE=@OLD_SQL_MODE;
+SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS;
+SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS;
