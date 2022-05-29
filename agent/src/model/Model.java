d7dc5b90560a58744fb149d15146dbc5328dd610
Yonatan Birman <jonatanbir10@gmail.com>
2022-05-29 14:43:30 +0300

    blabla

--- agent/src/model/Model.java
+++ agent/src/model/Model.java
@@ -2,6 +2,7 @@ package model;
 
 public class Model {
     public static void main(String[] args) {
-        System.out.println("Hello World!");        
+        System.out.println("Hello World!"); 
+        
     }
 }
