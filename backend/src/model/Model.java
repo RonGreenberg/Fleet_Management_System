6a3b425e5cd9a3b0cdb8b629e558e82e80488821
Ron Greenberg <ron.greenberg@cs.colman.ac.il>
2022-05-29 03:26:05 +0300

    Tabs to spaces fixed

--- backend/src/model/Model.java
+++ backend/src/model/Model.java
@@ -1,7 +1,7 @@
 package model;
 
 public class Model {
-	public static void main(String[] args) {
+    public static void main(String[] args) {
         System.out.println("Hello World!");        
-	}
+    }
 }
