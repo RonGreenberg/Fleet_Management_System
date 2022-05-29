95ad6de8b5c272c94a2cb76405f26ca60b204560
Ron Greenberg <ron.greenberg@cs.colman.ac.il>
2022-05-29 21:45:28 +0300

    Test

--- backend/src/interpreter/expression/Test.java
+++ backend/src/interpreter/expression/Test.java
@@ -8,7 +8,7 @@ public class Test {
         System.out.println(ExpressionEvaluator.evaluate("(3+5)*4-12"));
         System.out.println(ExpressionEvaluator.evaluate("7.5*(4.3+(6-12.3))/(0-3.0)"));
         System.out.println(ExpressionEvaluator.evaluate("5+2/(3-8)*5*2"));
-        System.out.println(ExpressionEvaluator.evaluate("-12+3*(1-8)-(-3)"));
+        System.out.println(ExpressionEvaluator.evaluate("-1+3*(1-8)-(-3)"));
     }
 
 }
