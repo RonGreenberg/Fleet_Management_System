29475020cdde5020d39518b5beeaf26b54b94287
SheerShveka <sheers2004@gmailcom>
2022-06-14 12:27:51 +0300

    ORM: Airplane

--- backend/src/orm/Airplane.java
+++ backend/src/orm/Airplane.java
@@ -1,4 +1,67 @@
 package orm;
 
+import java.util.Date;
+import java.util.Set;
+
 public class Airplane {
+    private String planeID;
+    private String model;
+    private java.util.Date dateAdded;
+    private float lastHeading;
+    private float lastAltitude;
+    private Set<Flight> flights;
+
+
+    Airplane()
+    {
+
+    }
+
+    public String getPlaneID() {
+        return planeID;
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public Date getDateAdded() {
+        return dateAdded;
+    }
+
+    public float getLastHeading() {
+        return lastHeading;
+    }
+
+    public float getLastAltitude() {
+        return lastAltitude;
+    }
+
+    public void setPlaneID(String planeID) {
+        this.planeID = planeID;
+    }
+
+    public void setModel(String model) {
+        this.model = model;
+    }
+
+    public void setDateAdded(Date dateAdded) {
+        this.dateAdded = dateAdded;
+    }
+
+    public void setLastHeading(float lastHeading) {
+        this.lastHeading = lastHeading;
+    }
+
+    public void setLastAltitude(float lastAltitude) {
+        this.lastAltitude = lastAltitude;
+    }
+
+    public void setFlights(Set<Flight> flights) {
+        this.flights = flights;
+    }
+
+    public Set<Flight> getFlights() {
+        return flights;
+    }
 }
