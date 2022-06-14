64ea6b32af51fcc3f9685621b2e529a5de77a828
SheerShveka <sheers2004@gmailcom>
2022-06-14 12:15:04 +0300

    ORM- I added the classes inside the package

---
+++ backend/src/orm/Flight.java
@@ -0,0 +1,36 @@
+package orm;
+
+public class Flight {
+    private int flightID;
+    private String planeID;
+    private String csvFileName;
+
+
+    Flight()
+    {
+    }
+
+    public void setFlightID(int flightID) {
+        this.flightID = flightID;
+    }
+
+    public void setCsvFileName(String csvFileName) {
+        this.csvFileName = csvFileName;
+    }
+
+    public void setPlaneID(String planeID) {
+        this.planeID = planeID;
+    }
+
+    public int getFlightID() {
+        return flightID;
+    }
+
+    public String getCsvFileName() {
+        return csvFileName;
+    }
+
+    public String getPlaneID() {
+        return planeID;
+    }
+}
