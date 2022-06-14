908f8aec59f169cb7bc47a5097cabc47957e9b75
SheerShveka <sheers2004@gmailcom>
2022-06-14 13:13:00 +0300

    ORM: HibernateUtil, QueriesUtil

--- backend/src/orm/HibernateUtil.java
+++ backend/src/orm/HibernateUtil.java
@@ -1,4 +1,26 @@
 package orm;
 
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
+
 public class HibernateUtil {
+    private SessionFactory sessionFactory;
+
+    private SessionFactory buildSesionFactory()
+    {
+        try {
+            // Create the SessionFactory from hibernate.cfg.xml
+            return new Configuration().configure().buildSessionFactory();
+        }
+        catch (Throwable ex) {
+            // Make sure you log the exception, as it might be swallowed
+            System.err.println("Initial SessionFactory creation failed." + ex);
+            throw new ExceptionInInitializerError(ex);
+        }
+    }
+
+    public SessionFactory getSessionFactory() {
+        return sessionFactory;
+    }
 }
--- backend/src/orm/QueriesUtil.java
+++ backend/src/orm/QueriesUtil.java
@@ -1,4 +1,46 @@
 package orm;
+import org.hibernate.query.Query;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
+import org.hibernate.cfg.Configuration;
+
+import java.util.List;
 
 public class QueriesUtil {
+    private SessionFactory sessionFactory;
+
+    public List<String> getAllPlaneIDs()
+    {
+        return null;
+    }
+
+    public Airplane getPlaneData(String planeID)
+    {
+
+        return null;
+    }
+
+    public List<Integer> getFlightIDs(String planeID,String fromDate)
+    {
+
+        return null;
+    }
+
+    public Flight getFlightDetails(int flightID)
+    {
+
+        return null;
+    }
+
+    public void saveFlight(Flight flight)
+    {
+
+    }
+
+    public int updatePlaneDetails(float lastHeading,float lastAltitude)
+    {
+
+        return 0;
+    }
 }
